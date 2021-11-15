80953f955e5aabfa8b21d666c4816b626986af53
wei.dai <wei.dai@leyantech.com>
2021-11-15 18:15:15 +0800

    nested json

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -9,17 +9,23 @@ import (
 )
 
 
+type event struct {
+    Title   string `json:"title"`
+    Content string `json:"content"`
+}
+
 type album struct {
     ID      string  `json:"id"`
     Title   string  `json:"title"`
     Artist  string  `json:"artist"`
     Price   float64 `json:"price"`
+    Events  []event   `json:"events"`
 }
 
 var albums = []album{
     {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
     {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99, Events: []event{{"a", "b"}, {"c", "d"}}},
 }
 
 func main() {
@@ -32,12 +38,13 @@ func main() {
 }
 
 func getAlbums(c *gin.Context) {
+    fmt.Println(albums)
     c.IndentedJSON(http.StatusOK, albums)
 }
 
 func postAlbums(c *gin.Context) {
     var newAlbum album
-    fmt.Println("lalala")
+    fmt.Println("inside post")
     /*
     buf, _ := ioutil.ReadAll(c.Request.Body)
     fmt.Println(buf)
@@ -46,6 +53,7 @@ func postAlbums(c *gin.Context) {
         fmt.Println("can't resolve album data")
         return
     }
+    fmt.Println(newAlbum)
     albums = append(albums, newAlbum)
     c.IndentedJSON(http.StatusCreated, newAlbum)
 }
